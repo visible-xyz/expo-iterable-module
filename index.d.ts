d3519e29619eca5c63e94b21d321ed6e17f030f2
bohdan-yashchuk <bohdan@visible.xyz>
2024-06-27 12:02:14 +0300

    added types

--- index.d.ts
+++ index.d.ts
@@ -5,4 +5,33 @@ declare module "expo-iterable-module" {
     InboxRowViewModel,
     InboxImpressionRowInfo,
   } from "@iterable/react-native-sdk";
+  export const PI: string;
+
+  export function hello(): string;
+
+  export {
+    Iterable,
+    IterableCommerceItem,
+    IterableConfig,
+    IterableInAppManager,
+    IterableAction,
+    IterableActionContext,
+    IterableLogLevel,
+    IterableInAppShowResponse,
+    IterableInAppTriggerType,
+    IterableInAppTrigger,
+    IterableInAppContentType,
+    IterableEdgeInsets,
+    IterableHtmlInAppContent,
+    IterableInboxMetadata,
+    IterableInAppLocation,
+    IterableInAppMessage,
+    IterableInAppCloseSource,
+    IterableInAppDeleteSource,
+    IterableInboxEmptyState,
+    IterableInboxMessageCell,
+    useAppStateListener,
+    useDeviceOrientation,
+    IterableDataRegion,
+  } from "@iterable/react-native-sdk";
 }
